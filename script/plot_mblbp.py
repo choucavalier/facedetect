79b87a79cfe4432efa436a583a5c1035c9c5111e
toogy <toogy@too.gy>
2016-09-15 19:24:22 +0200

    plot random mblbp features

--- script/plot_mblbp.py
+++ script/plot_mblbp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-'''Visualize mblbp features in a given window with an animation'''
+'''Visualize randomly chosen mblbp features in a given window'''
 
 import random
 import os
@@ -10,9 +10,6 @@ import matplotlib.patches as patches
 import matplotlib.animation as animation
 import matplotlib.image as mpimg
 from PIL import Image
-
-COLORS = ['#1a535c', '#4ecdc4', '#ff6b6b', '#ffe66d', '#ffe66d',
-          '#ff6b6b', '#4ecdc4', '#1a535c']
 
 def plot_animated_mblbp(window_w, window_h):
 
@@ -24,7 +21,7 @@ def plot_animated_mblbp(window_w, window_h):
 
     ax = plt.axes(xlim=(0, 20), ylim=(0, 20))
     ax.imshow(img, interpolation='none', cmap=plt.get_cmap('gray'),
-              extent=[0, 20, 20, 0], alpha=0.7)
+              extent=[0, 20, 20, 0])
     ax.set_ylim(ax.get_ylim()[::-1]) # invert y-axis
     ax.xaxis.tick_top() # move x-axis to the top
     ax.xaxis.set_ticks(range(1, 21))
@@ -41,19 +38,10 @@ def plot_animated_mblbp(window_w, window_h):
     for spine in ax.spines:
         ax.spines[spine].set_visible(False)
 
-    rectangles = []
-
-    for i in range(9):
-        rectangle = patches.Rectangle((0, 0), 0, 0,
-                                      alpha=0 if i == 4 else 0.6,
-                                      linewidth=0)
-        ax.add_patch(rectangle)
-        rectangles.append(rectangle)
-
     features = []
 
-    for block_w in range(3, window_w, 3):
-        for block_h in range(3, window_h, 3):
+    for block_w in range(3, 10, 3):
+        for block_h in range(3, 10, 3):
             for offset_x in range(window_w - block_w + 1):
                 for offset_y in range(window_h - block_h + 1):
                     feature = {
@@ -64,36 +52,23 @@ def plot_animated_mblbp(window_w, window_h):
                     }
                     features.append(feature)
 
+    colors = '1a535c-4ecdc4-ff6b6b-ffe66d-f7fff7-ffe66d-ff6b6b-4ecdc4-1a535c'
+    colors = ['#{}'.format(c) for c in colors.split('-')]
+    patterns = ['x', 'o', 'O', '*']
+
     random.shuffle(features)
+    sample = random.sample(features, 6)
 
-    def animate(i, features, rectangles):
+    for i, feature in enumerate(sample):
+        rectangle = patches.Rectangle(
+            (feature['offset_x'], feature['offset_y']),
+            feature['block_w'], feature['block_h'],
+            facecolor=colors[i], linewidth=1, alpha=0.4,
+            # hatch=random.choice(patterns)
+        )
+        ax.add_patch(rectangle)
 
-        color = random.choice(COLORS)
-
-        feature = features[i]
-
-        small_block_w = feature['block_w'] / 3
-        small_block_h = feature['block_h'] / 3
-
-        for i, rectangle in enumerate(rectangles):
-
-            rectangle.set_x(feature['offset_x'] + (i % 3) * small_block_w)
-            rectangle.set_y(feature['offset_y'] + (i // 3) * small_block_h)
-            rectangle.set_width(small_block_w)
-            rectangle.set_height(small_block_h)
-            rectangle.set_facecolor(color)
-
-        return rectangles
-
-    anim = animation.FuncAnimation(fig, animate, frames=120, interval=100,
-                                   blit=True, fargs=(features, rectangles))
-
-    anim.save('gfx/animated_mblbp.mp4', fps=6, bitrate=-1,
-              extra_args=['-vcodec', 'libx264', '-pix_fmt', 'rgb24'],
-              metadata={'frameon': False, 'pad_inches': 0.0,
-                        'bbox_inches': 'tight', 'dpi': dpi})
-
-    # plt.show()
+    plt.show()
 
 def main():
 
