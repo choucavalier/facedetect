c7adff220e518df9749be0f832947090f94a9a7b
Orkat <13577403+Orkat@users.noreply.github.com>
2016-10-02 22:49:39 +0200

    implemented training set repopulation

--- src/train.cc
+++ src/train.cc
@@ -99,20 +99,16 @@ static data_t load_data(const std::vector<mblbp_feature> &all_features,
     }
   }
 
-  std::size_t i = 0;
   for(auto& directory_entry : fs::directory_iterator(negative_path))
   {
-    if ( i >= positive_paths.size() )
-      break;
     negative_paths.push_back(directory_entry.path().string());
-    ++i;
   }
 
   std::cout << negative_paths.size() << " negative samples" << std::endl;
 
   #pragma omp parallel for
   //for(std::size_t i = 0; i < 100; ++i)
-  for(std::size_t i = 0; i < positive_paths.size(); ++i)
+  for(std::size_t i = 0; i < negative_paths.size(); ++i)
   {
     cv::Mat img, integral;
     img = cv::imread(negative_paths[i], CV_LOAD_IMAGE_GRAYSCALE);
@@ -131,6 +127,71 @@ static data_t load_data(const std::vector<mblbp_feature> &all_features,
   std::cout << "loading data took " << elapsed << "s" << std::endl;
 
   return data;
+}
+
+static data_t select_data_cascade(const data_t& training_set,
+                                  const mblbp_classifier &classifier)
+{
+  chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+
+  data_t positive_training_examples;
+  data_t negative_training_examples;
+
+  size_t n_pos = 0;
+  size_t n_neg = 0;
+
+  char classification_label;
+
+  for ( training_example : training_set )
+  {
+    classification_label = 1;
+    for(const auto& sc : classifier.strong_classifiers)
+    {
+      double sum = 0;
+      for(const auto& wc : sc.weak_classifiers)
+      {
+        int feature_value = training_example.first[wc.k];
+        sum += wc.regression_parameters[feature_value];
+      }
+      if(sum < 0)
+      {
+        classification_label = -1;
+        break;
+      }
+    }
+
+    if ( training_example.second == 1 )
+      ++n_pos;
+    else
+      ++n_neg;
+
+    if ( classification_label == 1 )
+    {
+      if ( training_example.second == 1 )
+        positive_training_examples.push_back( training_example );
+      else
+        negative_training_examples.push_back( training_example );
+    }
+  }
+
+  std::cout << "positive training examples passed through classification : " << positive_training_examples.size();
+  std::cout << " / " << n_pos << std::endl;
+  std::cout << "negative training examples passed through classification : " << negative_training_examples.size();
+  std::cout << " / " << n_neg << std::endl;
+
+  if ( positive_training_examples.size() < negative_training_examples.size() )
+    negative_training_examples.resize(positive_training_examples.size());
+  if ( negative_training_examples.size() < positive_training_examples.size() )
+    positive_training_examples.resize(negative_training_examples.size());
+
+  std::cout << "selected number of positive training examples : " << positive_training_examples.size() << std::endl;
+  std::cout << "selected number of negative training examples : " << negative_training_examples.size() << std::endl;
+
+  data_t selected_data;
+  selected_data.insert(selected_data.end(), positive_training_examples.begin(), positive_training_examples.end());
+  selected_data.insert(selected_data.end(), negative_training_examples.begin(), negative_training_examples.end());
+
+  return selected_data;
 }
 
 static void shuffle_data(data_t &data)
@@ -259,36 +320,40 @@ mblbp_classifier train(const std::string &positive_path,
     all_weak_classifiers.push_back(weak_classifier(all_features[k], k));
 
   std::cout << std::string(10, '-') << std::endl;
+
   std::cout << "calculating all MB-LBP features on all samples" << std::endl;
   data_t data = load_data(all_features, positive_path, negative_path);
-  std::cout << "shuffling data" << std::endl;
+  std::cout << "shuffling data of size : " << data.size() << std::endl;
   shuffle_data(data);
 
   std::cout << data.size() << " samples in dataset" << std::endl;
   std::cout << estimate_memory_size(data) << " bytes in memory" << std::endl;
 
   // split dataset
-  std::size_t split_idx = 2 * data.size() / 3;
+  std::size_t split_idx = 4 * data.size() / 5;
   data_t training_set(data.begin(), data.begin() + split_idx);
   data_t validation_set(data.begin() + split_idx, data.end());
 
   std::cout << training_set.size() << " samples in training set" << std::endl;
   std::cout << validation_set.size() << " samples in validation set"
             << std::endl;
+
   std::cout << std::string(10, '-') << std::endl;
 
   // current rates on validation set
   double detection_rate, tp_rate, tn_rate, fp_rate, fn_rate;
 
   int n_iter = 1;
-
+  size_t n_negative_examples = 0;
   do
   {
     std::cout << "iteration #" << n_iter << std::endl;
 
+    data_t iteration_training_set = select_data_cascade(training_set, classifier);
+
     // weights initialization to 1 / n_samples
-    std::vector<double> weights(training_set.size());
-    std::fill_n(weights.begin(), training_set.size(), 1.0 / training_set.size());
+    std::vector<double> weights(iteration_training_set.size());
+    std::fill_n(weights.begin(), iteration_training_set.size(), 1.0 / iteration_training_set.size());
 
     strong_classifier new_strong_classifier;
 
@@ -306,11 +371,11 @@ mblbp_classifier train(const std::string &positive_path,
         for(int j = 0; j < 256; ++j)
         {
           double numerator = 0, denominator = 0;
-          for(std::size_t i = 0; i < training_set.size(); ++i)
+          for(std::size_t i = 0; i < iteration_training_set.size(); ++i)
           {
-            if(training_set[i].first[wc.k] == j)
+            if(iteration_training_set[i].first[wc.k] == j)
             {
-              numerator += weights[i] * training_set[i].second;
+              numerator += weights[i] * iteration_training_set[i].second;
               denominator += weights[i];
             }
           }
@@ -320,10 +385,10 @@ mblbp_classifier train(const std::string &positive_path,
             wc.regression_parameters[j] = 0.5;
         }
         double wse = 0;
-        for(std::size_t i = 0; i < training_set.size(); ++i)
+        for(std::size_t i = 0; i < iteration_training_set.size(); ++i)
         {
-          double value = wc.regression_parameters[training_set[i].first[wc.k]];
-          wse += weights[i] * std::pow(value - training_set[i].second, 2);
+          double value = wc.regression_parameters[iteration_training_set[i].first[wc.k]];
+          wse += weights[i] * std::pow(value - iteration_training_set[i].second, 2);
         }
         #pragma omp critical(best_wse_update)
         {
@@ -361,8 +426,8 @@ mblbp_classifier train(const std::string &positive_path,
       double sum = 0;
       for(std::size_t i = 0; i < weights.size(); ++i)
       {
-        double value = bwc.regression_parameters[training_set[i].first[bwc.k]];
-        weights[i] = weights[i] * std::exp(-training_set[i].second * value);
+        double value = bwc.regression_parameters[iteration_training_set[i].first[bwc.k]];
+        weights[i] = weights[i] * std::exp(-iteration_training_set[i].second * value);
         sum += weights[i];
       }
       for(std::size_t i = 0; i < weights.size(); ++i)
@@ -398,17 +463,25 @@ mblbp_classifier train(const std::string &positive_path,
 
     remove_true_and_false_negatives(classifier, training_set);
 
+    n_negative_examples = 0;
+    for ( auto iter : iteration_training_set )
+    {
+      if ( iter.second != 1 )
+        ++n_negative_examples;
+    }
+
     std::cout << "detection_rate = " << detection_rate << std::endl;
     std::cout << "true positive = " << tp_rate << std::endl;
     std::cout << "true negative = " << tn_rate << std::endl;
     std::cout << "false positive = " << fp_rate << std::endl;
     std::cout << "false negative = " << fn_rate << std::endl;
-    std::cout << "remaining training set size = " << training_set.size() << std::endl;
+    std::cout << "training set size = " << iteration_training_set.size() << std::endl;
+    std::cout << "negative examples in training set : " << n_negative_examples << std::endl;
 
     n_iter++;
 
   } while(classifier.strong_classifiers.size() < train_n_strong &&
-          (detection_rate < target_detection_rate || fp_rate > target_fp_rate ) && training_set.size() > 0 );
+          (detection_rate < target_detection_rate || fp_rate > target_fp_rate ) && n_negative_examples > 0 );
 
   return classifier;
 }
